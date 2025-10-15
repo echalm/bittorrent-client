d67557c446ba5d748b232b20872fdc2c23b45973
echalm <echalm204@caledonian.ac.uk>
2025-10-15 12:36:21 +0100

    Grouping Messages Implemented

--- src/download.js
+++ src/download.js
@@ -20,3 +20,20 @@ function download(peer) {
         //handle response
     });
 }
+
+function onWholeMsg(socket, callback){
+    let savedBuf = Buffer.alloc(0);
+    let handshake = true;
+
+    socket.on('data', recvBuf => {
+        //calculates length of message
+        const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+        savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+        while (savedBuf.lenght >= 4 && savedBuf.length >= msgLen()){
+            callback(savedBuf.slice(0, msgLen()));
+            savedBuf = savedBuf.slice(msgLen());
+            handshake = false;
+        }
+    });
+}
